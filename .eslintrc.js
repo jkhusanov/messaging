dbe3b13fb0a54a01c3de102547ab3068a51a5844
Jkhusanov <khusanov.jakhongir@gmail.com>
2019-08-01 14:35:43 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: 'airbnb',
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  globals: {
+    fetch: false,
+    navigator: false,
+  },
+  plugins: ['react', 'jsx-a11y', 'import', 'react-hooks'],
+  rules: {
+    'no-use-before-define': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+    'react/sort-comp': 0,
+    'no-mixed-operators': 0,
+    'arrow-parens': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
